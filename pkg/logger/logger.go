ac0341fbca519cf35db0db4ef3aa584824d415d7
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-25 02:33:56 +0530

    added support for zerolog as default logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,3 +1,31 @@
-// Custom logging utility used all over Popcorn.
+// Custom logging utility used internally all over Popcorn.
 
 package logger
+
+import (
+	"Popcorn/internal/config"
+	"io"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+var output io.Writer
+var Logger zerolog.Logger
+
+func init() {
+	// setting configurations for logger
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	output = zerolog.ConsoleWriter{Out: os.Stdout}
+	// ConsoleWriter prettifies log, inefficient in prod
+	config.LoadDevConfig()
+	if os.Getenv("ENV") != "DEV" {
+		output = os.Stdout
+	}
+	// Finally, initialize the Logger with TimeStamp and CallStack features
+	Logger = zerolog.New(output).With().Timestamp().Caller().Stack().Logger()
+}

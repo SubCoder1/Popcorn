77a7f5aaae957d118047d89d51b5ff13f9e6603f
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-24 23:43:03 +0530

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,34 @@
+// This is the main
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Version indicates the current version of the application.
+var Version = "1.0.0"
+
+func init() {
+	currentDateTime := time.Now()
+	fmt.Println("Welcome to Popcorn!")
+	fmt.Printf("%d-%d-%d %d:%d:%d",
+		currentDateTime.Day(),
+		currentDateTime.Month(),
+		currentDateTime.Year(),
+		currentDateTime.Hour(),
+		currentDateTime.Minute(),
+		currentDateTime.Second(),
+	)
+}
+
+func main() {
+	server := gin.Default()
+
+	Router(server)
+
+	server.Run("localhost:8000")
+}
---
+++ cmd/server/router.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Router(router *gin.Engine) {}

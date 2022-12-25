d6d7a7496e6c0d8cf991dcf808cc2838e83782ba
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-25 15:16:53 +0530

    Fixed file not found issue in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 
 // uses go package: godotenv to load up development enviroment variables
 func LoadDevConfig() {
-	err := godotenv.Load("config/dev.env")
+	err := godotenv.Load("../../config/dev.env")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)

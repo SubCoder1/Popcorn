0e7bca6f2c54b4cb35317754f43c1a2638f0f3f9
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-25 17:30:13 +0530

    Added default route "/"

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -3,7 +3,14 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func Router(router *gin.Engine) {}
+func Router(router *gin.Engine) {
+	// This is the route to default path
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to Popcorn!")
+	})
+}

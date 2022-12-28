8c371a698ab5ac34ff0ef15ad2048c450514e915
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-28 16:41:57 +0530

    Added UserModel

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,3 +1,9 @@
 // Structure of User Model in Popcorn.
 
 package user
+
+type User struct {
+	ID       string `json:"id,omitempty"`
+	Username string `json:"username" validate:"required,ascii,min=5,max=20,excludesall=' '"`
+	Password string `json:"password" validate:"required,min=6"`
+}

38ba53f942817101e075c10c159626ce9cd2eafe
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-28 16:41:42 +0530

    Added usergroup in router

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -3,14 +3,22 @@
 package main
 
 import (
+	"Popcorn/internal/user"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Router(router *gin.Engine) {
-	// This is the route to default path
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Welcome to Popcorn!")
+	// This is the route to default path.
+	router.GET("/", func(gctx *gin.Context) {
+		gctx.String(http.StatusOK, "Welcome to Popcorn!")
 	})
+
+	// User related route groups.
+	usergroup := router.Group("/user")
+	{
+		// Route to register users on Popcorn
+		usergroup.POST("/register", user.RegisterUser())
+	}
 }

2c5af8df936207301e9e3f084bbfadfe0447f7fa
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-29 16:28:00 +0530

    Validations used internally are moved to error pkg

--- pkg/validation/validate.go
+++ pkg/validation/validate.go
@@ -1,4 +1,4 @@
-// Handles all sorts of custom data validations and post-validation responses happening in Popcorn.
+// Handles all sorts of custom data validations happening in Popcorn.
 
 package validation
 
@@ -9,15 +9,8 @@ import (
 	"github.com/asaskevich/govalidator"
 )
 
-// Standard for Validation-error responses
-type validationResponse struct {
-	Param   string `json:"param"`   // Parameter or Field
-	Message string `json:"message"` // Issue in Field
-}
-
-func CustomValidationTags() {
+func RegisterCustomValidations() {
 	// This custom validation checks if there's any spaces in the input string.
-	// Currently being used in the UserModel.
 	govalidator.TagMap["nospace"] = govalidator.Validator(func(str string) bool {
 		return !strings.Contains(str, " ")
 	})
@@ -38,22 +31,4 @@ func CustomValidationTags() {
 		}
 		return hasChar && hasNum
 	})
-}
-
-// Scans through set of validation errors found by govalidator
-// Generates a slice of serializable validationResponse
-func GenerateValidationErrorJSON(errs []error) []validationResponse {
-	// govalidator returns array of errors -> Param:Message
-	// We split the error from ":"
-	resp := []validationResponse{}
-	for _, err := range errs {
-		e := strings.Split(err.Error(), ":")
-		resp = append(
-			resp, validationResponse{
-				Param:   e[0],
-				Message: strings.TrimSpace(e[1]),
-			},
-		)
-	}
-	return resp
 }

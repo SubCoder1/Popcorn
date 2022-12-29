d4ae799e5f2c6609af54acc81ecac4fe270b7ba9
SubCoder1 <ghoshsoumakanti@gmail.com>
2022-12-30 01:40:24 +0530

    Changed structure of db connector to follow clean code

--- pkg/db/connector.go
+++ pkg/db/connector.go
@@ -3,7 +3,9 @@
 package db
 
 import (
-	"Popcorn/pkg/logger"
+	"Popcorn/internal/errors"
+	logger "Popcorn/pkg/log"
+	"context"
 	"os"
 	"strconv"
 	"strings"
@@ -11,23 +13,47 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-// A DAO (Data-Access-Object) to be used internally all over Popcorn.
-var redisDAO *redis.Client
+// RedisDB represents a redis client connection to be used internally in Popcorn.
+type RedisDB struct {
+	client *redis.Client
+}
 
-func init() {
+// Client returns the redis client wrapped by RedisDB.
+func (db *RedisDB) Client() *redis.Client {
+	return db.client
+}
+
+// Returns a new Redis DB connection wrapped up by RedisDB struct.
+func NewDBConnection(client *redis.Client) *RedisDB {
 	dbNumber, strerr := strconv.Atoi(strings.TrimSpace(os.Getenv("REDIS_DB_NUMBER")))
 	if strerr != nil {
 		logger.Logger.Fatal().Err(strerr).Msg("Couldn't parse ENV: REDIS_DB_NUMBER")
 	}
 	// Initializing a connection to Redis-server.
-	redisDAO = redis.NewClient(&redis.Options{
+	client = redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_ADDR") + ":" + os.Getenv("REDIS_PORT"),
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       dbNumber,
 	})
+	return &RedisDB{client: client}
 }
 
-// This function closes the redisDAO client, should be called before closing the server.
-func CloseDBConnection() error {
-	return redisDAO.Close()
+// Helper to check connection status of redis client to redis-server.
+// Equivalent to a PING request on redis-server, returns PONG on success.
+func (db *RedisDB) CheckDBConnection(ctx context.Context) error {
+	logger.Logger.Info().Msg("Checking DB Connection . . .")
+	// Pinging the Redis-server to check connection status
+	cnterr := db.Client().Ping(ctx).Err()
+	if cnterr != nil {
+		// Most likely, DB connection failure
+		return errors.InternalServerError(cnterr.Error())
+	}
+	// Connection successful
+	logger.Logger.Info().Msg("Connection to DB Successful")
+	return nil
+}
+
+// Helper to close the RedisDB client, should be called before closing the server.
+func (db *RedisDB) CloseDBConnection() error {
+	return db.Client().Close()
 }

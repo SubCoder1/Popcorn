555933f1a6ea5d45ad12d0819b0cd0e045115080
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-02 20:24:58 +0530

    Added userID as a field in UserModel

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -4,6 +4,7 @@ package entity
 
 // Tags: json -> serialization, redis -> serialize from db output, valid -> validations
 type User struct {
+	ID       uint64 `json:"id" redis:"id" valid:"-"`
 	Username string `json:"username" redis:"username" valid:"required,type(string),printableascii,stringlength(5|20),nospace~username:No spaces allowed here"`
 	Password string `json:"password" redis:"password" valid:"required,type(string),minstringlength(5),pwdstrength~password:At least 1 letter and 1 number is mandatory"`
 }

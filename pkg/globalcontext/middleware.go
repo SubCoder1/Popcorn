584fe98868042bd969ab51787ab08eca29f8c612
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-04 22:10:30 +0530

    Added unique ID functionality as a middleware

---
+++ pkg/globalcontext/middleware.go
@@ -0,0 +1,24 @@
+// Context middleware is used as a global middleware to populate request context with unique ID.
+// This ID will be helpful in debugging issues happening for a request in handler chain.
+
+package globalcontext
+
+import (
+	"Popcorn/pkg/log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+// This middleware will be used to populate every incoming request's context with an Unique UUID.
+// This middleware will be used as a global one.
+func UniqueIDMiddleware(logger log.Logger) gin.HandlerFunc {
+	return func(gctx *gin.Context) {
+		rqId, uuiderr := uuid.NewRandom()
+		if uuiderr != nil {
+			logger.Error().Err(uuiderr).Msg("Error during generating UUID for ReqID.")
+		} else {
+			gctx.Set("ReqID", rqId.String())
+		}
+	}
+}

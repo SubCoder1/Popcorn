6b771345a74b7d2c88c645d6e329283a7bed3585
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-15 22:53:02 +0530

    Added ascii validation in user model

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -6,6 +6,6 @@ package entity
 type User struct {
 	ID       uint64 `json:"id" redis:"id" valid:"-"`
 	Username string `json:"username" redis:"username" valid:"required,type(string),printableascii,stringlength(5|20),nospace~username:No spaces allowed here"`
-	FullName string `json:"full_name" redis:"full_name" valid:"required,type(string),stringlength(5|30),alphawithspace~full_name:Couldn't validate Full Name"`
+	FullName string `json:"full_name" redis:"full_name" valid:"required,type(string),stringlength(5|30),ascii,alphawithspace~full_name:Couldn't validate Full Name"`
 	Password string `json:"password" redis:"password" valid:"required,type(string),minstringlength(5),pwdstrength~password:At least 1 letter and 1 number is mandatory"`
 }

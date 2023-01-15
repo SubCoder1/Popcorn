a28b598ef730fbc956b71dad7b602ed07df91cbb
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-16 01:00:16 +0530

    Added UserLogin sub-model

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -9,3 +9,9 @@ type User struct {
 	FullName string `json:"full_name" redis:"full_name" valid:"required,type(string),stringlength(5|30),ascii,alphawithspace~full_name:Couldn't validate Full Name"`
 	Password string `json:"password" redis:"password" valid:"required,type(string),minstringlength(5),pwdstrength~password:At least 1 letter and 1 number is mandatory"`
 }
+
+// Sub-Model of User, used to bind login data from client
+type UserLogin struct {
+	Username string `json:"username" valid:"required,type(string),printableascii,stringlength(5|20),nospace~username:No spaces allowed here"`
+	Password string `json:"password" redis:"password" valid:"required,type(string),minstringlength(5),pwdstrength~password:At least 1 letter and 1 number is mandatory"`
+}

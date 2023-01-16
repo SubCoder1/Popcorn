f171fae143cd086076af17d087642c02cc8319f6
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-16 15:30:15 +0530

    Globally used middlewares moved to pkg/middlewares

---
+++ pkg/middlewares/cors.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// This middleware handles CORS policy for Popcorn server.
+func CORSMiddleware(addr string) gin.HandlerFunc {
+	return func(gctx *gin.Context) {
+		gctx.Writer.Header().Set("Access-Control-Allow-Origin", addr)
+		gctx.Writer.Header().Set("Vary", "Origin")
+		gctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		gctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		gctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if gctx.Request.Method == "OPTIONS" {
+			gctx.AbortWithStatus(204)
+			return
+		}
+
+		gctx.Next()
+	}
+}

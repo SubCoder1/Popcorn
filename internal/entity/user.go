88e7a282f509c15edf1034b1a3c355e59fb3a2f4
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-18 16:28:57 +0530

    Added omitempty in FullName to be binderr safe

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -10,7 +10,7 @@ import (
 // Saved in DB as user:<gang_name>
 type User struct {
 	Username   string `json:"username" redis:"username" valid:"required,type(string),printableascii,stringlength(5|20),nospace~username:No spaces allowed here"`
-	FullName   string `json:"full_name" redis:"full_name" valid:"type(string),stringlength(5|30),ascii,alphawithspace~full_name:Couldn't validate Full Name,optional"`
+	FullName   string `json:"full_name,omitempty" redis:"full_name" valid:"type(string),stringlength(5|30),ascii,alphawithspace~full_name:Couldn't validate Full Name,optional"`
 	Password   string `json:"password" redis:"password" valid:"required,type(string),minstringlength(5),pwdstrength~password:At least 1 letter and 1 number is mandatory"`
 	ProfilePic string `json:"user_profile_pic,omitempty" redis:"user_profile_pic" valid:"-"`
 }

82ce6b94264e2ca14737538a470210712beb52ba
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-21 23:33:41 +0530

    Added redis struct tag to MembersListKey

--- internal/entity/gang.go
+++ internal/entity/gang.go
@@ -8,11 +8,11 @@ type Gang struct {
 	Name           string `json:"gang_name" redis:"gang_name" valid:"required,type(string),printableascii,stringlength(5|20),nospaceonly~gang_name:Gang Name cannot contain only spaces"`
 	PassKey        string `json:"gang_pass_key" redis:"gang_pass_key" valid:"required,type(string),minstringlength(5)"`
 	Limit          uint   `json:"gang_member_limit" redis:"gang_member_limit" valid:"required,range(2|10)"`
-	MembersListKey string `json:"gang_members_key,omitempty" valid:"-"`
+	MembersListKey string `json:"gang_members_key,omitempty" redis:"gang_members_key" valid:"-"`
 	Created        int64  `json:"gang_created,omitempty" redis:"gang_created" valid:"-"`
 }
 
 // Saved in DB as gang-members:<gang.Admin>
 type GangMembersList struct {
-	MembersList []string `json:"gang_member_list,omitempty" valid:"-"`
+	MembersList []string `json:"gang_member_list,omitempty" redis:"gang_member_list" valid:"-"`
 }

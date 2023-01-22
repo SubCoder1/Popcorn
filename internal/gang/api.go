2d38ccff8bd94e3483849088eaa82a2452b6b2aa
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-22 20:43:20 +0530

    Added API to handle user created or joined gang data

--- internal/gang/api.go
+++ internal/gang/api.go
@@ -16,10 +16,11 @@ func GangHandlers(router *gin.Engine, service Service, AuthWithAcc gin.HandlerFu
 	gangGroup := router.Group("/api/gang")
 	{
 		gangGroup.POST("/create", AuthWithAcc, create_gang(service, logger))
+		gangGroup.GET("/get", AuthWithAcc, get_gang(service, logger))
 	}
 }
 
-// createGang returns a handler which takes care of creating gangs in Popcorn.
+// create_gang returns a handler which takes care of creating gangs in Popcorn.
 func create_gang(service Service, logger log.Logger) gin.HandlerFunc {
 	return func(gctx *gin.Context) {
 		var gang entity.Gang
@@ -54,8 +55,32 @@ func create_gang(service Service, logger log.Logger) gin.HandlerFunc {
 			gctx.JSON(err.Status, err)
 			return
 		}
-		gctx.JSON(http.StatusOK, gin.H{
-			"gang": gang,
-		})
+		gctx.Status(http.StatusOK)
+	}
+}
+
+// get_gang returns a handler which takes care of getting user created or joined gangs in Popcorn.
+func get_gang(service Service, logger log.Logger) gin.HandlerFunc {
+	return func(gctx *gin.Context) {
+		// Fetch username from context which will be used as the gang admin
+		username, ok := gctx.Value("Username").(string)
+		if !ok {
+			// Type assertion error
+			logger.WithCtx(gctx).Error().Msg("Type assertion error in get_gang")
+			gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
+		}
+		data, err := service.getgang(gctx, username)
+		if err != nil {
+			// Error occured, might be validation or server error
+			err, ok := err.(errors.ErrorResponse)
+			if !ok {
+				// Type assertion error
+				gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
+			}
+			gctx.JSON(err.Status, err)
+			return
+		}
+
+		gctx.JSON(http.StatusOK, data)
 	}
 }

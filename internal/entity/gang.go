8013dc3de7155785c1886f68b3df61233d9a1218
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-24 13:14:57 +0530

    Added gangKey and gangSearch sub-entity

--- internal/entity/gang.go
+++ internal/entity/gang.go
@@ -12,7 +12,20 @@ type Gang struct {
 	Created        int64  `json:"gang_created,omitempty" redis:"gang_created" valid:"-"`
 }
 
-// Saved in DB as gang-members:<gang.Admin>
+// Saved in DB as gang-members:<members>
 type GangMembersList struct {
 	MembersList []string `json:"gang_member_list,omitempty" redis:"gang_member_list" valid:"-"`
+}
+
+// Used to bind and validate join_gang request
+type GangKey struct {
+	Admin string `json:"gang_admin,omitempty" valid:"required,type(string),printableascii,stringlength(5|20),nospace~username:No spaces allowed here"`
+	Name  string `json:"gang_name" valid:"required,type(string),printableascii,stringlength(5|20),nospaceonly~gang_name:Gang Name cannot contain only spaces"`
+	Key   string `json:"gang_key,omitempty" valid:"-"`
+}
+
+// Used to bind and validate search_gang request
+type GangSearch struct {
+	Name   string `valid:"required,type(string),printableascii,stringlength(1|20),nospaceonly~gang_name:Gang Name cannot contain only spaces"`
+	Cursor int    `valid:"-"`
 }

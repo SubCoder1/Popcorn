abdce6b8d4a0806a69d9888cdcf92d0fda01b37f
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-24 13:15:24 +0530

    Added support for search and join gang in Popcorn

--- internal/gang/api.go
+++ internal/gang/api.go
@@ -7,6 +7,7 @@ import (
 	"Popcorn/internal/errors"
 	"Popcorn/pkg/log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +18,8 @@ func GangHandlers(router *gin.Engine, service Service, AuthWithAcc gin.HandlerFu
 	{
 		gangGroup.POST("/create", AuthWithAcc, create_gang(service, logger))
 		gangGroup.GET("/get", AuthWithAcc, get_gang(service, logger))
+		gangGroup.POST("/join", AuthWithAcc, join_gang(service, logger))
+		gangGroup.GET("/search", AuthWithAcc, search_gang(service, logger))
 	}
 }
 
@@ -62,14 +65,14 @@ func create_gang(service Service, logger log.Logger) gin.HandlerFunc {
 // get_gang returns a handler which takes care of getting user created or joined gangs in Popcorn.
 func get_gang(service Service, logger log.Logger) gin.HandlerFunc {
 	return func(gctx *gin.Context) {
-		// Fetch username from context which will be used as the gang admin
+		// Fetch username from context which will be used in getgang service
 		username, ok := gctx.Value("Username").(string)
 		if !ok {
 			// Type assertion error
 			logger.WithCtx(gctx).Error().Msg("Type assertion error in get_gang")
 			gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
 		}
-		data, err := service.getgang(gctx, username)
+		data, canCreate, canJoin, err := service.getgang(gctx, username)
 		if err != nil {
 			// Error occured, might be validation or server error
 			err, ok := err.(errors.ErrorResponse)
@@ -81,6 +84,85 @@ func get_gang(service Service, logger log.Logger) gin.HandlerFunc {
 			return
 		}
 
-		gctx.JSON(http.StatusOK, data)
+		gctx.JSON(http.StatusOK, gin.H{
+			"gang":          data,
+			"canCreateGang": canCreate,
+			"canJoinGang":   canJoin,
+		})
+	}
+}
+
+// join_gang returns a handler which takes care of joining a gang in Popcorn.
+func join_gang(service Service, logger log.Logger) gin.HandlerFunc {
+	return func(gctx *gin.Context) {
+		// Fetch username from context which will be used as the joingang service
+		username, ok := gctx.Value("Username").(string)
+		if !ok {
+			// Type assertion error
+			logger.WithCtx(gctx).Error().Msg("Type assertion error in get_gang")
+			gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
+		}
+		var gangKey entity.GangKey
+		// Serialize received data into GangKey struct
+		if binderr := gctx.BindJSON(&gangKey); binderr != nil {
+			// Error occured during serialization
+			gctx.JSON(http.StatusUnprocessableEntity, errors.UnprocessableEntity(""))
+			return
+		}
+		// Set gang-key which is of format gang:<gang_admin>
+		gangKey.Key = "gang:" + gangKey.Admin
+		err := service.joingang(gctx, username, gangKey)
+		if err != nil {
+			// Error occured, might be validation or server error
+			err, ok := err.(errors.ErrorResponse)
+			if !ok {
+				// Type assertion error
+				gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
+			}
+			gctx.JSON(err.Status, err)
+			return
+		}
+		gctx.Status(http.StatusOK)
+	}
+}
+
+// search_gang returns a handler which takes care of gang search in Popcorn.
+func search_gang(service Service, logger log.Logger) gin.HandlerFunc {
+	return func(gctx *gin.Context) {
+		// Fetch username from context which will be used in getgang service
+		username, ok := gctx.Value("Username").(string)
+		if !ok {
+			// Type assertion error
+			logger.WithCtx(gctx).Error().Msg("Type assertion error in get_gang")
+			gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
+		}
+
+		var query entity.GangSearch
+		gang_name := gctx.DefaultQuery("gang_name", "")
+		cursor, converr := strconv.Atoi(gctx.DefaultQuery("cursor", "0"))
+		if converr != nil || cursor < 0 || cursor > 1000 {
+			// Invalid cursor input
+			gctx.Status(http.StatusBadRequest)
+			return
+		}
+		// bind data into query struct
+		query.Name = gang_name
+		query.Cursor = cursor
+
+		response, newCursor, err := service.searchgang(gctx, query, username)
+		if err != nil {
+			// Error occured, might be validation or server error
+			err, ok := err.(errors.ErrorResponse)
+			if !ok {
+				// Type assertion error
+				gctx.JSON(http.StatusInternalServerError, errors.InternalServerError(""))
+			}
+			gctx.JSON(err.Status, err)
+			return
+		}
+		gctx.JSON(http.StatusOK, gin.H{
+			"result": response,
+			"page":   newCursor,
+		})
 	}
 }

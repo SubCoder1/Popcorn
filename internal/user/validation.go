f5e3cbcaa2413b7f86ba639699a45cedf38ef7c0
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-25 13:10:59 +0530

    Added new validations for user and gang entity

--- internal/user/validation.go
+++ internal/user/validation.go
@@ -1,17 +1,18 @@
-// All custom validations related to gang entity in Popcorn are defined here.
+// All custom validations related to user entity in Popcorn are defined here.
 
 package user
 
 import (
 	"Popcorn/pkg/log"
 	"context"
+	"regexp"
 	"unicode"
 
 	"github.com/asaskevich/govalidator"
 )
 
 func RegisterCustomValidations(ctx context.Context, logger log.Logger) {
-	// This custom validation checks for user-set password strength.
+	// Password strength validation.
 	// Only checks for 1 letter and 1 number, nothing too complicated.
 	govalidator.TagMap["pwdstrength"] = govalidator.Validator(func(pwd string) bool {
 		hasChar, hasNum := false, false
@@ -27,6 +28,19 @@ func RegisterCustomValidations(ctx context.Context, logger log.Logger) {
 			}
 		}
 		return hasChar && hasNum
+	})
+	// Username validation.
+	// Username can only contain letters, numbers, underscores & periods.
+	govalidator.TagMap["username_custom"] = govalidator.Validator(func(str string) bool {
+		pattern := regexp.MustCompile("[^a-zA-Z0-9_.]")
+		return !pattern.MatchString(str)
+	})
+
+	// Fullname validation.
+	// Fullname can only contain letters, numbers & spaces.
+	govalidator.TagMap["fullname_custom"] = govalidator.Validator(func(str string) bool {
+		pattern := regexp.MustCompile("[^a-zA-Z ]")
+		return !pattern.MatchString(str) && !govalidator.HasWhitespaceOnly(str)
 	})
 
 	logger.WithCtx(ctx).Info().Msg("Successfully registered user related custom validations.")

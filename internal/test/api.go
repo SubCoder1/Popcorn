e0fd65b7bd57ba22ecee85c4580bdd9a1c090bf7
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-02-06 18:36:22 +0530

    Added response to be sent after tests complete

--- internal/test/api.go
+++ internal/test/api.go
@@ -3,6 +3,7 @@ package test
 import (
 	"Popcorn/pkg/log"
 	"bytes"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,27 +14,56 @@ import (
 
 // Format of Request helper TestAPI() handles
 type RequestAPITest struct {
-	Method       string            // Method of API request - [GET, POST, PUT, DELETE . . .]
-	Path         string            // API Path
-	Body         *bytes.Reader     // Request Body
-	WantResponse []int             // Expected Response according to request
-	Headers      map[string]string // Request headers
+	Method       string         // Method of API request - [GET, POST, PUT, DELETE . . .]
+	Path         string         // API Path
+	Body         *bytes.Reader  // Request Body
+	WantResponse []int          // Expected Response according to request
+	Header       http.Header    // Request headers
+	Cookie       []*http.Cookie // Request Cookies
+}
+
+// Response attached with every APITest
+type APIResponse struct {
+	Header http.Header    // Response headers
+	Cookie []*http.Cookie // Response cookies
+	Body   []byte         // Response body in []bytes unmarshallable to specific struct
+}
+
+// Helper to attach a mock header
+func MockHeader() http.Header {
+	header := http.Header{}
+	header.Add("Content-Type", "application/json")
+	return header
 }
 
 // Helper to execute API tests in Popcorn.
-func ExecuteAPITest(logger log.Logger, t *testing.T, router *gin.Engine, request RequestAPITest) {
+func ExecuteAPITest(logger log.Logger, t *testing.T, router *gin.Engine, request *RequestAPITest) APIResponse {
 	// Setup the test request
 	req, reqerr := http.NewRequest(request.Method, request.Path, request.Body)
-	for key, val := range request.Headers {
-		req.Header.Set(key, val)
-	}
 	if reqerr != nil {
 		// Error in NewRequest
 		logger.Error().Err(reqerr).Msg("Error occured during calling NewRequest in ExecuteAPITest()")
+		t.Error()
+		return APIResponse{}
+	}
+	req.Header = request.Header
+	for _, cookie := range request.Cookie {
+		req.AddCookie(cookie)
 	}
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
-	// t.Log(w.Body)
+
 	// Assert the response
 	assert.Contains(t, request.WantResponse, w.Code)
+
+	// Attach Response Body
+	response := w.Result()
+	defer response.Body.Close()
+	data, _ := io.ReadAll(response.Body)
+
+	return APIResponse{
+		Header: response.Header.Clone(),
+		Cookie: response.Cookies(),
+		Body:   data,
+	}
 }

41f5de8ed5b24e2b7d3a426112eb456b85986761
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-02-16 17:11:02 +0530

    Added global validation "nospace"

---
+++ pkg/validations/validation.go
@@ -0,0 +1,18 @@
+// All global custom validations in Popcorn are defined here.
+// These validations are allowed to be used anywhere in the application.
+
+package validations
+
+import (
+	"context"
+	"log"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func RegisterCustomValidations(ctx context.Context, logger log.Logger) {
+	// This global validation doesn't allow whitespace in input.
+	govalidator.TagMap["nospace"] = govalidator.Validator(func(str string) bool {
+		return !govalidator.HasWhitespace(str)
+	})
+}

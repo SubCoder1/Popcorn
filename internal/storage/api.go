b1f60ccb93ec66a85f3c409bd511e3fb435cb17e
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-05-23 03:19:51 +0530

    Added DELETE handler to content upload termination

--- internal/storage/api.go
+++ internal/storage/api.go
@@ -3,16 +3,16 @@
 package storage
 
 import (
-	"Popcorn/internal/gang"
 	"Popcorn/pkg/log"
 
 	"github.com/gin-gonic/gin"
+	tusd "github.com/tus/tusd/pkg/handler"
 )
 
-func APIHandlers(router *gin.Engine, gangRepo gang.Repository, authWithAcc gin.HandlerFunc, logger log.Logger) {
-	storage_handler := GetTusdStorageHandler(gangRepo, logger)
-	router.POST("/api/upload_content", authWithAcc, gin.WrapF(storage_handler.PostFile))
-	router.GET("/api/upload_content/:id", authWithAcc, gin.WrapF(storage_handler.GetFile))
-	router.HEAD("/api/upload_content/:id", authWithAcc, gin.WrapF(storage_handler.HeadFile))
-	router.PATCH("/api/upload_content/:id", authWithAcc, gin.WrapF(storage_handler.PatchFile))
+func APIHandlers(router *gin.Engine, storage_handler *tusd.UnroutedHandler, authWithAcc, validateAdmin gin.HandlerFunc, logger log.Logger) {
+	router.POST("/api/upload_content", authWithAcc, validateAdmin, gin.WrapF(storage_handler.PostFile))
+	router.GET("/api/upload_content/:id", authWithAcc, validateAdmin, gin.WrapF(storage_handler.GetFile))
+	router.HEAD("/api/upload_content/:id", authWithAcc, validateAdmin, gin.WrapF(storage_handler.HeadFile))
+	router.PATCH("/api/upload_content/:id", authWithAcc, validateAdmin, gin.WrapF(storage_handler.PatchFile))
+	router.DELETE("/api/upload_content/:id", authWithAcc, validateAdmin, gin.WrapF(storage_handler.DelFile))
 }

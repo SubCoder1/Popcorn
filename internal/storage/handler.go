cfb9309da57825f23c33564ca349aaebd95b018d
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-08-20 23:01:18 +0530

    Added code to clean up unstreamed file leftovers

--- internal/storage/handler.go
+++ internal/storage/handler.go
@@ -6,10 +6,13 @@ import (
 	"Popcorn/internal/entity"
 	"Popcorn/internal/gang"
 	"Popcorn/internal/sse"
+	"Popcorn/pkg/cleanup"
 	"Popcorn/pkg/log"
 	"context"
 	"errors"
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/h2non/filetype"
 	"github.com/tus/tusd/pkg/filestore"
@@ -85,9 +88,35 @@ func GetTusdStorageHandler(gangRepo gang.Repository, sseService sse.Service, log
 			user := hook.HTTPRequest.Header.Get("User")
 			dberr := gangRepo.UpdateGangContentData(ctx, logger, user, hook.Upload.MetaData["filename"], hook.Upload.ID, false)
 			if dberr != nil {
-				// Error occured in EraseGangContentData()
+				// Error occured in UpdateGangContentData()
 				return tusd.NewHTTPError(errors.New("internal server error"), 500)
 			}
+
+			// The uploaded file should be deleted if not streamed under 10mins as storage is limited
+			time.AfterFunc(10*time.Minute, func() {
+				gangKey := "gang:" + user
+				gang, _ := gangRepo.GetGang(ctx, logger, gangKey, user, false)
+
+				if len(gang.Name) != 0 && !gang.Streaming {
+					logger.Info().Msgf(fmt.Sprintf("Deleting unstreamed content files for: %s", gangKey))
+					// Delete gang content files
+					cleanup.DeleteContentFiles(gang.ContentID, logger)
+					// Erase gang content data from DB
+					gangRepo.UpdateGangContentData(ctx, logger, user, "", "", false)
+					// Notify the members that stream has stopped
+					members, _ := gangRepo.GetGangMembers(ctx, logger, user)
+					for _, member := range members {
+						go func(member string) {
+							data := entity.SSEData{
+								Data: nil,
+								Type: "gangUpdate",
+								To:   member,
+							}
+							sseService.GetOrSetEvent(ctx).Message <- data
+						}(member)
+					}
+				}
+			})
 			return nil
 		},
 	})
